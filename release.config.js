8c7cb4c545dda4fc4d15510acd2be693b97ee322
Erik Slovák <34162967+erik-slovak@users.noreply.github.com>
2023-10-17 09:54:07 +0200

    chore: update github actions (#78)
    * chore: update github actions
    
    * chore: return deleted workflow
    
    * chore: add support for prereleases
    
    * chore: add prerelase branches to release.yml
    
    * chore: add workflow for syncing README with code
    
    * chore: remove commitizen
    
    * chore: remove commitizen config
    
    * chore: use txo configs for semantic-release and commitlint
    
    * chore: update yarn.lock

--- release.config.js
+++ release.config.js
@@ -5,49 +5,7 @@
 **/
 
 module.exports = {
-  branches: [
-    'main',
-  ],
-  plugins: [
-    ['@semantic-release/commit-analyzer', {
-      present: 'conventionalcommits',
-      releaseRules: [
-        { breaking: true, release: 'major' },
-        { revert: true, release: 'patch' },
-        { type: 'docs', release: 'patch' },
-        { type: 'feat', release: 'minor' },
-        { type: 'fix', release: 'patch' },
-        { type: 'perf', release: 'patch' },
-        { type: 'refactor', release: 'patch' },
-        { scope: 'no-release', release: false },
-      ],
-    }],
-    ['@semantic-release/release-notes-generator', {
-      preset: 'conventionalcommits',
-      presetConfig: {
-        types: [
-          { type: 'build', section: 'Build system / dependencies' },
-          { type: 'ci', section: 'CI' },
-          { type: 'docs', section: 'Documentation' },
-          { type: 'feat', section: 'Features' },
-          { type: 'fix', section: 'Bug fixes' },
-          { type: 'perf', section: 'Performance' },
-          { type: 'refactor', section: 'Refactoring' },
-          { type: 'test', section: 'Testing' },
-        ],
-      },
-    }],
-    ['semantic-release-slack-bot',
-      {
-        notifyOnSuccess: true,
-        notifyOnFail: true,
-      },
-    ],
-    '@semantic-release/changelog',
-    '@semantic-release/npm',
-    '@semantic-release/github',
-    ['@semantic-release/git', {
-      assets: ['CHANGELOG.md', 'package.json'],
-    }],
+  extends: [
+    '@txo/semantic-release',
   ],
 }
